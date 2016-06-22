fdb84afd6d42f957cdc7326751679d4260f113bb
Alec Snyder <linuxbash8@gmail.com>
2016-06-22 11:48:56 -0700

    wait on startup for server spool up closes #17

--- example/testapp/apitest/features/support/feature_hooks.js
+++ example/testapp/apitest/features/support/feature_hooks.js
@@ -2,6 +2,22 @@
 var cucumberApi = require('cucumber-api');
 var Promise = require('bluebird');
 var childProcess = Promise.promisifyAll(require('child_process'), {multiArgs: true});
+var portUsed = require('tcp-port-used');
+
+waitForStartup = function (done) {
+  portUsed.check(3000, "localhost")
+  .then(function(inUse) {
+    if (inUse) return done();
+    setTimeout(function () {
+      waitForStartup(done);
+    }, 50);
+  })
+  .catch( function (err) {
+    console.log("Cannot connect to server. Please make sure that your ports are configured properly");
+    done(err);
+  });
+}
+
 
 module.exports = function () {
   var self = this;
@@ -9,7 +25,7 @@ module.exports = function () {
   this.registerHandler('BeforeFeatures', function (event, done) {
     self.serverPID = childProcess.exec("../src/serv")
     /* Start application and database here */
-    done()
+    waitForStartup(done);
   });
 
 

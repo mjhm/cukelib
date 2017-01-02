20e73e52ca3511aea173a995064172b15db673e8
Mac McLaughlin <macmclaughlin@john-mclaughlins-computer.local>
2017-01-02 01:05:17 -0800

    minor fix

--- src/utilities/cuke2compat.js
+++ src/utilities/cuke2compat.js
@@ -1,13 +1,25 @@
 // Boilerplate code that upgrades a cucumber@1.x file to cucumber@2.x
 // Put the following line at the end of a step file:
 // require('../utilities/cuke2compat')(module.exports);
-
+//
+// It can also be used to upgrade a cucumber@1.x tag array to an appropriate tag string. Example:
+// const cuke2compat = require('cuke2compat')
+// ...
+// this.Before(cuke2compat(['tag1', tag2], doThisBeforeHook);
+//
+const _ = require('lodash');
 const cucumber = require('cucumber');
 
 module.exports = function (modexp) {
   if (cucumber.defineSupportCode) {
-    cucumber.defineSupportCode((context) => {
-      modexp.call(context);
-    });
+    if (_.isFunction(modexp)) {
+      cucumber.defineSupportCode((context) => {
+        modexp.call(context);
+      });
+    }
+    if (_.isArray(modexp)) {
+      return modexp.join(' or ');
+    }
   }
+  return modexp;
 };

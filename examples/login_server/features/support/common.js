5aa1b10d969769a3f726d1b151f210600c4a9dbd
John McLaughlin <john.mclaughlin@originate.com>
2017-02-05 07:57:00 -0800

    minor base cleanup

--- examples/login_server/features/support/common.js
+++
@@ -1,7 +0,0 @@
-const { requestSupport } = require('cukelib');
-
-module.exports = function () {
-  this.Given(/^user "([^"]*)" logs in using password "([^"]*)"$/, (email, password) => {
-    requestSupport.requestPOST({ email, password });
-  });
-};
--- examples/login_server/features/users.feature
+++ examples/login_server/features/users.feature
@@ -4,4 +4,4 @@ Feature: User Endpoints
     Given POST "/login"
       | { email: 'peggy@scdp.com', password: 'stanRizz0' } |
 
-  Scenario: Attempt to access /users/current before login
+  Scenario: Test basic response of /users/current
